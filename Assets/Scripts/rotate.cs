03a1b4329038b6f1450ddbf3c804a5e41a993778
Muhammed Cesur <90576168+Muhammed-cesur@users.noreply.github.com>
2023-07-08 21:12:04 +0300

    final ve boss kısmı hazırlandı

---
+++ Assets/Scripts/rotate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+    public float YrotationSpeed = 0.01f; 
+    public float XrotationSpeed = 0.0f; 
+
+
+    private float y = 0.0f;
+    private float x = 0.0f;
+
+
+    // Use this for initialization
+    void Start () {
+        
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        transform.localRotation = Quaternion.Euler(x, y, 0);
+        y += YrotationSpeed; 
+		x += XrotationSpeed;
+    }
+}
