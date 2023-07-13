4445c4d6da842909ee3dad1b3f4e9cc3da621e2d
AtaGn <ata_gn@hotmail.com>
2023-07-14 00:19:52 +0300

    Save System

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -11,25 +11,40 @@ public class PlayerHealth : MonoBehaviour
     public int currentHealth;  // Current health of the player
     public Slider healthBar;
 
-
+    private Vector3 savedPosition;  // Saved position of the player
 
     private void Start()
     {
-        currentHealth = maxHealth; // Başlangıçta canı maksimum değere ayarla
+        currentHealth = maxHealth; // Set current health to maximum at the start
         UpdateHealthSlider();
+    }
 
+    private void Update()
+    {
+        // Check for the player's death
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+
+        // Handle saving and loading inputs
+        if (Input.GetKeyDown(KeyCode.F5))
+        {
+            SavePlayerData();
+        }
+        else if (Input.GetKeyDown(KeyCode.F9))
+        {
+            LoadPlayerData();
+        }
     }
 
     public void TakeDamage(int damageAmount)
     {
-        currentHealth -= damageAmount; // Hasarı mevcut cana uygula
+        currentHealth -= damageAmount; // Apply damage to the current health
         Debug.Log(currentHealth);
         UpdateHealthSlider();
-        if (currentHealth <= 0f)
-        {
-            Die();
-        }
     }
+
     private void UpdateHealthSlider()
     {
         if (healthBar != null)
@@ -39,17 +54,60 @@ public class PlayerHealth : MonoBehaviour
             Debug.Log(healthBar.value);
         }
     }
+
     private void Die()
     {
-        // Oyuncu ölme durumunda yapılması gereken işlemler
+        // Actions to perform when the player dies
         Debug.Log("Player has died.");
+        LoadPlayerData();
+        // Respawn the player with full health
+        Respawn();
+    }
 
-        // Örneğin, oyunu yeniden başlat veya ölüm ekranını göster gibi işlemler yapılabilir.
+    private void Respawn()
+    {
+        // Reset the player's position to the saved position
+        transform.position = savedPosition;
+
+        // Reset the player's health to full
+        ResetHealth();
     }
 
     public void ResetHealth()
     {
         currentHealth = maxHealth;
+        UpdateHealthSlider();
+    }
+
+    public void SavePlayerData()
+    {
+        // Save the player's position and health
+        savedPosition = transform.position;
+        PlayerPrefs.SetFloat("SavedPositionX", savedPosition.x);
+        PlayerPrefs.SetFloat("SavedPositionY", savedPosition.y);
+        PlayerPrefs.SetFloat("SavedPositionZ", savedPosition.z);
+        PlayerPrefs.SetInt("SavedHealth", currentHealth);
+
+        Debug.Log("Player data saved.");
+    }
+
+    public void LoadPlayerData()
+    {
+        // Load the saved player's position and health
+        float savedPositionX = PlayerPrefs.GetFloat("SavedPositionX");
+        float savedPositionY = PlayerPrefs.GetFloat("SavedPositionY");
+        float savedPositionZ = PlayerPrefs.GetFloat("SavedPositionZ");
+        savedPosition = new Vector3(savedPositionX, savedPositionY, savedPositionZ);
+
+        // Set the player's position
+        transform.position = savedPosition;
+
+        currentHealth = PlayerPrefs.GetInt("SavedHealth");
+        UpdateHealthSlider();
+
+        Debug.Log("Player data loaded.");
+
+        // Restart the current scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
-
